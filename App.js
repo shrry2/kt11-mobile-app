bd139324e2f508813bc04e8fed20c480b556da18
Takaki Takeuchi <takaki@takeu.ch>
2020-06-24 00:37:21 +0900

    Implemented basic tab navigation

--- App.js
+++ App.js
@@ -1,19 +1,90 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+import HomeScreen from './screens/Home';
+
+function SearchScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Search!</Text>
+    </View>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Account!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            switch (route.name) {
+              case 'Home':
+                iconName = 'home';
+                break;
+              case 'Search':
+                iconName = 'search1';
+                break;
+              case 'Account':
+                iconName = 'user'
+                break;
+              default:
+                iconName = 'questioncircleo';
+                break;
+            }
+            return <AntDesign name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'green',
+          inactiveTintColor: 'gray',
+          showLabel: false,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{　title: 'ホーム'　}}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{　title: '検索'　}}
+        />
+        <Tab.Screen
+          name="Account"
+          component={AccountScreen}
+          options={{　title: 'アカウント'　}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    color: '#fff',
   },
+  title: {
+    color: '#fff',
+    fontSize: 40,
+  }
 });
